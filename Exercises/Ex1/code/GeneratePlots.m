3e61902e81a1a3b8d44552fa243681b6aa24728a
einar90 <einar.baumann@gmail.com>
2014-01-17 15:41:51 +0100

    Started case 4

--- Exercises/Ex1/code/GeneratePlots.m
+++ Exercises/Ex1/code/GeneratePlots.m
@@ -24,21 +24,25 @@ plot(ANL250(800,:));
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%% CASE 2                                             %%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-EXP250 = load("250µ_EXP.dat");
-EXP500 = load("500µ_EXP.dat");
-EXP750 = load("750µ_EXP.dat");
-EXP1000 = load("1000µ_EXP.dat");
+EXP250  = load('250µ_EXP.dat');
+EXP500  = load('500µ_EXP.dat');
+EXP750  = load('750µ_EXP.dat');
+EXP1000 = load('1000µ_EXP.dat');
 %EXP1250  % Unstable
 
 figure(2);
 title('Case 2: Explicit, dt towards unstable');
 hold on;
+
 plot(EXP250(1,:),'color','red');
 plot(EXP250(length(EXP250),:),'color','red')
+
 plot(EXP500(1,:),'color','green');
 plot(EXP500(length(EXP500),:),'color','green')
+
 plot(EXP750(1,:),'color','blue');
 plot(EXP750(length(EXP750),:),'color','blue')
+
 plot(EXP1000(1,:),'color','black');
 plot(EXP1000(length(EXP1000),:),'color','black')
 
@@ -72,3 +76,20 @@ end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%% CASE 4                                             %%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+IMP500   = load('500µ_IMP.dat');
+IMP5000  = load('5000µ_IMP.dat');
+IMP50000 = load('50000µ_IMP.dat');
+
+figure(3);
+title('Case 4: Implicit, increasing time steps');
+hold on;
+
+plot(IMP500(1,:),'color','red');
+plot(IMP500(length(IMP500),:),'color','red')
+
+plot(IMP5000(1,:),'color','green');
+plot(IMP5000(length(IMP5000),:),'color','green')
+
+plot(IMP50000(1,:),'color','blue');
+plot(IMP50000(length(IMP50000),:),'color','blue')
